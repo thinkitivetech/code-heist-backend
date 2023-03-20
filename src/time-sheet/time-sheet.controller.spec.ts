20dda46b03232292cbad02efb76639472fc4bd2e
Abhay Ahire <abhay.ahire@thinkitive.com>
2023-03-20 16:01:25 +0530

    fix  : add test classes

---
+++ src/time-sheet/time-sheet.controller.spec.ts
@@ -0,0 +1,100 @@
+import { Test } from '@nestjs/testing';
+import { HttpStatus } from '@nestjs/common';
+import { TimeSheetController } from './time-sheet.controller';
+import { TimeSheetService } from './time-sheet.service';
+import { GetTimeSheetReq, PatchTimeSheetReq, TimeSheetStatusReq } from './dto/time-sheet-dto';
+
+describe('TimeSheetController', () => {
+  let timeSheetController: TimeSheetController;
+  let timeSheetService: TimeSheetService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [TimeSheetController],
+      providers: [TimeSheetService],
+    }).compile();
+
+    timeSheetService = moduleRef.get<TimeSheetService>(TimeSheetService);
+    timeSheetController = moduleRef.get<TimeSheetController>(TimeSheetController);
+  });
+
+  describe('status', () => {
+    it('should update time sheet status', async () => {
+      const timeSheetReq: any = { };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      jest.spyOn(timeSheetService, 'updateStatus').mockResolvedValue(undefined);
+
+      await timeSheetController.status(timeSheetReq, response);
+
+      expect(timeSheetService.updateStatus).toHaveBeenCalledWith(timeSheetReq, response);
+      expect(response.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(response.json).toHaveBeenCalledWith({
+        success: true,
+        message: 'Time sheet status updated successfully',
+        data: null,
+      });
+    });
+
+    it('should return error response if service throws an error', async () => {
+      const timeSheetReq: any = { };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const error = new Error('Test error');
+
+      jest.spyOn(timeSheetService, 'updateStatus').mockRejectedValue(error);
+
+      await timeSheetController.status(timeSheetReq, response);
+
+      expect(timeSheetService.updateStatus).toHaveBeenCalledWith(timeSheetReq, response);
+      expect(response.status).toHaveBeenCalledWith(HttpStatus.INTERNAL_SERVER_ERROR);
+      expect(response.json).toHaveBeenCalledWith({
+        success: false,
+        message: 'It seems there is some technical glitch at our end, Unable to un',
+        error_code: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: error.message,
+      });
+    });
+  });
+
+  describe('getTimeSheet', () => {
+    it('should get time sheet', async () => {
+      const timeSheetRequest: any = { };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      jest.spyOn(timeSheetService, 'getTimeSheet').mockResolvedValue(undefined);
+
+      await timeSheetController.getTimeSheet(timeSheetRequest, response);
+
+      expect(timeSheetService.getTimeSheet).toHaveBeenCalledWith(timeSheetRequest, response);
+      expect(response.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(response.json).toHaveBeenCalledWith({
+        success: true,
+        message: 'Time sheet fetched successfully',
+        data: null,
+      });
+    });
+
+    it('should return error response if service throws an error', async () => {
+      const timeSheetRequest: any = { };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const error = new Error('Test error');
+
+      jest.spyOn(timeSheetService, 'getTimeSheet').mockRejectedValue(error);
+
+      await timeSheetController.getTimeSheet(timeSheetRequest, response);
+
+    });
+  })
+})
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,100 @@
+import { HttpStatus, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { HttpError } from 'routing-controllers';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entity/user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/userModel/create-user.dto';
 
 describe('UserService', () => {
   let service: UserService;
 
+  let userService: UserService;
+  let userRepository: Repository<UserEntity>;
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
+
+    userService = moduleRef.get<UserService>(UserService);
+    userRepository = moduleRef.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getUsers', () => {
+    it('should return a paginated list of users', async () => {
+      const expectedData = [{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }];
+      const req = { limit: 10, page: 1 };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnValue({ success: true, data: expectedData }),
+      };
+      const createQueryBuilderSpy = jest
+        .spyOn(userRepository, 'createQueryBuilder')
+        .mockReturnValue({
+          andWhere: jest.fn().mockReturnThis(),
+          orderBy: jest.fn().mockReturnThis(),
+          skip: jest.fn().mockReturnThis(),
+          take: jest.fn().mockReturnThis(),
+          getManyAndCount: jest.fn().mockReturnValue(expectedData),
+        } as any);
+
+      const result = await userService.getUsers(req, response);
+
+      expect(result).toEqual({
+        success: true,
+        data: expectedData,
+      });
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('user');
+      expect(response.status).toHaveBeenCalledWith(201);
+      expect(response.json).toHaveBeenCalledWith({
+        success: true,
+        data: expectedData,
+      });
+    });
+
+    it('should handle errors', async () => {
+      const req = { limit: 10, page: 1 };
+      const response = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnValue({ success: false }),
+      };
+      const createQueryBuilderSpy = jest
+        .spyOn(userRepository, 'createQueryBuilder')
+        .mockReturnValue({
+          andWhere: jest.fn().mockReturnThis(),
+          orderBy: jest.fn().mockReturnThis(),
+          skip: jest.fn().mockReturnThis(),
+          take: jest.fn().mockReturnThis(),
+          getManyAndCount: jest.fn().mockRejectedValue(new Error()),
+        } as any);
+      const loggerErrorSpy = jest.spyOn(userService['logger'], 'error');
+
+      expect(async () => {
+        await userService.getUsers(req, response);
+      }).rejects.toThrowError();
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('user');
+      expect(response.status).toHaveBeenCalledWith(500);
+      expect(response.json).toHaveBeenCalledWith({
+        success: false,
+      });
+      expect(loggerErrorSpy).toHaveBeenCalled();
+    });
   });
-});
+
+
+
+})  

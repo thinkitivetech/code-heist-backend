d551950db9ad72e95956eaabd5d45478044cf323
Abhay Ahire <abhay.ahire@thinkitive.com>
2023-03-20 17:00:25 +0530

    fix  : refactor code

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -45,118 +45,68 @@ export class ProjectService {
     }
   }
 
-  public async getAllProject(
-    reqProject: ReqProject,
-    @Res() response: any
-  ): Promise<any[]> {
+  public async getAllProject(reqProject: any, @Res() response: any): Promise<any[]> {
     try {
       this.logger.log(`Got request to fetch project details by params`);
-      const userDetail = await this.userRepo.findOne({
-        where: { id: reqProject.userId },
+      const userDetail = await this.userRepo.findOne({ where: { id: reqProject.userId } });
+  
+      const roleMapping : any = {
+        [UserRoles.TEAM_LEAD]: "teamLeadId",
+        [UserRoles.MANAGER]: "mangerId",
+        [UserRoles.ENGINEER]: "engineerId",
+        [UserRoles.SALES]: "salesId",
+      };
+  
+      if (userDetail?.role && roleMapping[userDetail.role]) {
+        reqProject.filter[roleMapping[userDetail.role]] = userDetail.id;
+      }
+  
+      const selectQuery = this.projectRepository.createQueryBuilder("project");
+  
+      const filters = [
+        { field: "engineerId", value: reqProject.filter.engineerId },
+        { field: "teamLeadId", value: reqProject.filter.teamLeadId },
+        { field: "managerId", value: reqProject.filter.mangerId },
+        { field: "projectId", value: reqProject.filter.projectId },
+        { field: "salesId", value: reqProject.filter.salesId },
+        { field: "companyId", value: reqProject.filter.companyId },
+        { field: "status", value: reqProject.filter.status },
+      ];
+  
+      filters.forEach(({ field, value }) => {
+        if (value) {
+          selectQuery.andWhere(`project.${field} = :${field}`, { [field]: value });
+        }
       });
-
-      if (userDetail?.role === UserRoles.TEAM_LEAD) {
-        reqProject.filter.teamLeadId = userDetail.id;
-      }
-      if (userDetail?.role === UserRoles.MANAGER) {
-        reqProject.filter.mangerId = userDetail.id;
-      }
-      if (userDetail?.role === UserRoles.ENGINEER) {
-        reqProject.filter.engineerId = userDetail.id;
-      }
-      if (userDetail?.role === UserRoles.SALES) {
-        reqProject.filter.salesId = userDetail.id;
-      }
-
-      let selectQuery = this.projectRepository.createQueryBuilder("project");
-
-      if (reqProject && reqProject.filter) {
-        if(reqProject.filter.engineerId) {
-          selectQuery.andWhere("project.engineerId = :engineerId", {
-            engineerId: reqProject.filter.engineerId,
-          })
-        }
-
-        if(reqProject.filter.teamLeadId) {
-          selectQuery.andWhere("project.teamLeadId = :teamLeadId", {
-            teamLeadId: reqProject.filter.teamLeadId,
-          })
-        }
-
-        if(reqProject.filter.mangerId) {
-          selectQuery.andWhere("project.mangerId = :mangerId", {
-            teamLeadId: reqProject.filter.mangerId,
-          })
-        }
-
-        if(reqProject.filter.projectId) {
-          selectQuery.andWhere("project.id = :projectId", {
-            projectId: reqProject.filter.projectId,
-          })
-        }
-
-        if(reqProject.filter.salesId) {
-          selectQuery.andWhere("project.salesId = :salesId", {
-            salesId: reqProject.filter.salesId,
-          })
-        }
-
-        if(reqProject.filter.companyId) {
-          selectQuery.andWhere("project.companyId = :companyId", {
-            companyId: reqProject.filter.companyId,
-          })
-        }
-
-        if(reqProject.filter.status) {
-          selectQuery.andWhere("project.status = :status", {
-            status: reqProject.filter.status,
-          })
-        }
-      }
-
+  
       if (reqProject.startTime) {
-        selectQuery.andWhere("project.createdAt >= :createdAt", {
-          createdAt: reqProject.startTime,
-        });
+        selectQuery.andWhere("project.createdAt >= :createdAt", { createdAt: reqProject.startTime });
       }
       if (reqProject.endTime) {
-        selectQuery.andWhere("project.createdAt <= :createdAt", {
-          createdAt: reqProject.endTime,
-        });
+        selectQuery.andWhere("project.createdAt <= :createdAt", { createdAt: reqProject.endTime });
       }
-      if (reqProject && reqProject.filter && reqProject.filter.order) {
-        if (reqProject.filter.order === "DESC") {
-          selectQuery.orderBy("project.createdAt", "DESC");
-        }
-        if (reqProject.filter.order === "ASC") {
-          selectQuery.orderBy("project.createdAt", "ASC");
-        }
+      if (reqProject.filter?.order) {
+        const direction = reqProject.filter.order === "ASC" ? "ASC" : "DESC";
+        selectQuery.orderBy("project.createdAt", direction);
       }
-      if (reqProject && reqProject.limit && reqProject.page) {
-        selectQuery
-          .skip(reqProject.limit * (reqProject.page - 1))
-          .take(reqProject.limit);
-      } else {
-        reqProject.page = 1;
-        reqProject.limit = 10;
-      }
-
-      const data = await selectQuery.getManyAndCount();
-      const paginatedResponse = paginateResponse(
-        data,
-        reqProject.limit,
-        reqProject.page
-      );
+  
+      reqProject.page = reqProject.page || 1;
+      reqProject.limit = reqProject.limit || 10;
+  
+      selectQuery.skip(reqProject.limit * (reqProject.page - 1)).take(reqProject.limit);
+  
+      const [data, count] = await selectQuery.getManyAndCount();
+      const paginatedResponse = paginateResponse(data, reqProject.limit, reqProject.page);
+  
       return response.status(HttpStatus.OK).json({
         success: true,
-        message: "User has been created successfully",
+        message: "Projects have been fetched successfully",
         data: paginatedResponse,
+        count,
       });
     } catch (err) {
-      this.logger.error(
-        `Error while fetching user for id ${reqProject} Err as ${err}`
-      );
+      this.logger.error(err);
       throw new Error(`No Project has been found with ${JSON.stringify(err)}`);
     }
-  }
+  }      
 }
